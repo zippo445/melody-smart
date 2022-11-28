81e19504c5a2a702324566dc1300989375f5c407
Philippe Jean <phil@philippe-jean.com>
2022-11-27 23:23:33 -0500

    initial

---
+++ Sources/MelodySmart/IontoManager.swift
@@ -0,0 +1,59 @@
+//
+//  File.swift
+//  
+//
+//  Created by Philippe Jean on 2022-11-27.
+//
+
+import Foundation
+
+
+public struct DeviceDescription {
+    var id: String;
+    
+    init(id: String){
+        self.id = id;
+    }
+}
+
+public class IontoDevice {
+    
+    var initialized = false;
+    var scanning = false;
+    
+    func scan () {
+        scanning = true;
+        
+    }
+    
+    func stopScan () {
+        self.scanning = false;
+    }
+    
+    public func isScanning () -> Bool {
+        return self.scanning;
+    }
+    
+    func initialize() -> String {
+        if (initialized){
+            return "Already initialized";
+        }
+        initialized = true;
+        
+        let msge = "Using Dummy MelodyManager" ;
+        print(msge)
+        
+        return msge;
+    }
+
+    
+    
+
+    func getDevices() -> [DeviceDescription]{
+        
+        var devices = [DeviceDescription]();
+        
+      
+        return devices;
+    }
+}
